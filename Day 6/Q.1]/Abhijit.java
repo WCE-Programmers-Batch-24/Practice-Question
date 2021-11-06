c84f1522b63fbeab13ce63239ab36c7dacaa5912
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-11-06 17:48:05 +0530

    Java solution of Day 6 Q.1

---
+++ Day 6/Q.1]/Abhijit.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in) ;
+		int t = sc.nextInt() ;
+		
+		while (t-- > 0)
+		{
+		    long n = sc.nextLong(), s = sc.nextLong() ;
+		    
+		    long ans = n*(n+1)/2 - s ;
+		    
+		    if (ans > 0 && ans <= n) System.out.println(ans) ;
+		    else System.out.println(-1) ;
+		}
+		
+	}
+}

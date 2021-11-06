a38569d5d661439fb4006c233bd59cd340cfcccc
RAO.exe <84059138+RAOexe@users.noreply.github.com>
2021-11-06 22:04:53 +0530

    Update RAO.cpp

--- Day 6/Q.2]/RAO.cpp
+++ Day 6/Q.2]/RAO.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.siz(), chunks = 0, max = -1;
+        int n = arr.size(), chunks = 0, max = -1;
         for(int i=0; i<n; i++){
             if(max < arr[i]) max = arr[i];
           

a9943a9caf51b31e250fe794d383122fbf81a584
RAO.exe <84059138+RAOexe@users.noreply.github.com>
2021-11-09 21:16:00 +0530

    Add: Solution of third question

---
+++ Day 9/Q.3]/RAO.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    
+    while(1){
+        ll t;
+        cin>>t;
+        
+        if(t == 42) break;
+        else{
+            cout<<t<<endl;
+        }
+    }
+    return 0;
+}

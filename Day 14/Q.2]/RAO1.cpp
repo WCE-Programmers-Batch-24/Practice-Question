7ce0f3a16e2b9d38acf7a0007572253fd29453ac
RAO.exe <84059138+RAOexe@users.noreply.github.com>
2021-11-16 18:10:41 +0530

    Add: Comments

--- Day 14/Q.2]/RAO1.cpp
+++ Day 14/Q.2]/RAO1.cpp
@@ -5,13 +5,17 @@ public:
         vector<int> even;
         
         for(int i = 0; i < A.size(); i++){
+            // pushing even numbers in vector 'even' 
             if(A[i]%2==0){
                 even.push_back(A[i]);
-            }else{
+            }
+            // pushing odd numbers in vector 'odd'
+            else{
                 odd.push_back(A[i]);
             }
         }
         
+        // merging to arrays - even + odd
         even.insert(even.end(), odd.begin(), odd.end());
         return even;
     }

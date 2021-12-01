b464ffb90942f0b762741057aaccb56501776c93
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2021-12-01 18:07:55 +0530

    Create Ravi.cpp

---
+++ Day 22/Q.1]/Ravi.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        sort(arr,arr+n);
+        cout<<arr[(n+k)/2]<<endl;
+
+    }
+    return 0;
+}

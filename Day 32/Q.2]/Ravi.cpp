bb21e0732abe8b75fc8249f88b6f43e6d308af99
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2021-12-09 12:17:11 +0530

    Create Ravi.cpp

---
+++ Day 32/Q.2]/Ravi.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n = arr.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1])
+                ans += arr[i] - arr[i - 1];
+        }
+        return ans;
+    }
+};
